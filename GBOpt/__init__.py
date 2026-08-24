f4b3ba06518b76410d1d6616f3a9ddc07a9f8d66
Jarin French <jarin.french@inl.gov>
2026-08-24 08:49:20 -0600

    Switch to pyproject.toml. Closes #93

--- GBOpt/__init__.py
+++ GBOpt/__init__.py
@@ -1,7 +1,6 @@
 # Copyright 2025, Battelle Energy Alliance, LLC, ALL RIGHTS RESERVED
 
-__version__ = "0.2.0"
-
+from GBOpt._version import __version__
 from GBOpt.Atom import Atom
 from GBOpt.GBMaker import GBMaker
 from GBOpt.GBManipulator import GBManipulator
---
+++ GBOpt/_version.py
@@ -0,0 +1,3 @@
+# Copyright 2025, Battelle Energy Alliance, LLC, ALL RIGHTS RESERVED
+
+__version__ = "0.2.0"
